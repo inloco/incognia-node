35d9612c5793533cc166b293bebda13cf920574f
Gileno Oliveira <gileno.oliveira@inloco.com.br>
2021-03-12 11:30:03 -0300

    chore(rollup): Add rollup and initial packages

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  external: ['axios']
+}

1e9aad911094e5a10326d96ec01571a5a2b45229
Gileno Oliveira <gileno.oliveira@inloco.com.br>
2021-03-15 14:58:05 -0300

    feature(formatting): Add snake_case and camelCase convert functions

---
+++ src/formatting.test.js
@@ -0,0 +1,26 @@
+import {
+  convertObjectToCamelCase,
+  convertObjectToSnakeCase
+} from './formatting'
+
+describe('convertObjectToSnakeCase', () => {
+  it('converts keys to snake_case, even if nested', () => {
+    expect(
+      convertObjectToSnakeCase({
+        keyName: { nestedKey: 2 },
+        a: [{ nestedInArray: 1 }]
+      })
+    ).toEqual({ key_name: { nested_key: 2 }, a: [{ nested_in_array: 1 }] })
+  })
+})
+
+describe('convertObjectToCamelCase', () => {
+  it('converts keys to camelCase, even if nested', () => {
+    expect(
+      convertObjectToCamelCase({
+        key_name: { nested_key: 2 },
+        a: [{ nested_in_array: 1 }]
+      })
+    ).toEqual({ keyName: { nestedKey: 2 }, a: [{ nestedInArray: 1 }] })
+  })
+})

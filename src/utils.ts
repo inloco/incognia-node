716fb9e0067651e041a5ec0bf65627f2ce6e8743
Marcel Rebouças <marcel.reboucas@incognia.com>
2022-07-19 09:14:55 -0300

    refactor(utils): refactor the user agent method

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,6 @@ import os from 'os'
 import { version } from './version'
 
 export const buildUserAgent = () => {
-  const lib = 'incognia-node'
-  const libVersion = version
-
   const platform = os.platform()
   const platformVersion = os.release()
   const platformArch = os.arch()
@@ -12,5 +9,5 @@ export const buildUserAgent = () => {
   const nodeVersion = process.version
 
   // Example: incognia-node/4.0.1 (darwin 21.2.0 x64) Node/v16.15.1
-  return `${lib}/${libVersion} (${platform} ${platformVersion} ${platformArch}) Node/${nodeVersion}`
+  return `incognia-node/${version} (${platform} ${platformVersion} ${platformArch}) Node/${nodeVersion}`
 }
